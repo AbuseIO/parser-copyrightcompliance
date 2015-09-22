fcd749a9294c52f6c2b78c7ee86d6c540d2c3810
Bart Vrancken <bart@abuse.io>
2015-09-22 10:18:55 +0200

    fix parser

--- src/Copyrightcompliance.php
+++ src/Copyrightcompliance.php
@@ -50,63 +50,63 @@ class Copyrightcompliance extends Parser
             } else {
                 continue;
             }
+        }
 
-            // Sadly their report is not consistantly an attachment and might end up in the body so we need to fallback
-            // to a body XML search if there was nothing found in attachments.
-            if (empty($xmlReport)) {
-                preg_match(
-                    '/\<\?xml.*/s',
-                    $this->parsedMail->getMessageBody(),
-                    $regs
-                );
-                $xmlReport = $regs[0];
+        // Sadly their report is not consistantly an attachment and might end up in the body so we need to fallback
+        // to a body XML search if there was nothing found in attachments.
+        if (empty($xmlReport)) {
+            preg_match(
+                '/\<\?xml.*\<\/Infringement\>/s',
+                $this->parsedMail->getMessageBody(),
+                $regs
+            );
+            $xmlReport = $regs[0];
+        }
+
+        if (!empty($xmlReport) && $xmlReport = simplexml_load_string($xmlReport)) {
+            $feedName = 'default';
+
+            // Create a corrected array
+            $xmlReport = json_encode($xmlReport);
+            $xmlReport = json_decode($xmlReport, true);
+
+            // Unset fields that are really crap
+            unset($xmlReport['Source']['SubType']);
+
+            // Apply filters from configuration
+            foreach (config("{$configBase}.feeds.{$feedName}.filters") as $filter) {
+                unset($xmlReport[$filter]);
             }
 
-            if (!empty($xmlReport) && $xmlReport = simplexml_load_string($xmlReport)) {
-                $feedName = 'default';
+            $fields = $xmlReport['Source'];
+            $columns = array_filter(config("{$configBase}.feeds.{$feedName}.fields"));
 
-                // Create a corrected array
-                $xmlReport = json_encode($xmlReport);
-                $xmlReport = json_decode($xmlReport, true);
-
-                // Unset fields that are really crap
-                unset($xmlReport['Source']['SubType']);
-
-                // Apply filters from configuration
-                foreach (config("{$configBase}.feeds.{$feedName}.filters") as $filter) {
-                    unset($xmlReport[$filter]);
-                }
-
-                $fields = $xmlReport['Source'];
-                $columns = array_filter(config("{$configBase}.feeds.{$feedName}.fields"));
-
-                if (count($columns) > 0) {
-                    foreach ($columns as $column) {
-                        if (!isset($fields[$column])) {
-                            return $this->failed(
-                                "Required field ${column} is missing in the report or config is incorrect."
-                            );
-                        }
+            if (count($columns) > 0) {
+                foreach ($columns as $column) {
+                    if (!isset($fields[$column])) {
+                        return $this->failed(
+                            "Required field ${column} is missing in the report or config is incorrect."
+                        );
                     }
                 }
-
-                if (config("{$configBase}.feeds.{$feedName}.enabled") !== true) {
-                    return $this->success($events);
-                }
-
-                $event = [
-                    'source'        => config("{$configBase}.parser.name"),
-                    'ip'            => $fields['IP_Address'],
-                    'domain'        => false,
-                    'uri'           => false,
-                    'class'         => config("{$configBase}.feeds.{$feedName}.class"),
-                    'type'          => config("{$configBase}.feeds.{$feedName}.type"),
-                    'timestamp'     => strtotime($fields['TimeStamp']),
-                    'information'   => json_encode($xmlReport),
-                ];
-
-                $events[] = $event;
             }
+
+            if (config("{$configBase}.feeds.{$feedName}.enabled") !== true) {
+                return $this->success($events);
+            }
+
+            $event = [
+                'source'        => config("{$configBase}.parser.name"),
+                'ip'            => $fields['IP_Address'],
+                'domain'        => false,
+                'uri'           => false,
+                'class'         => config("{$configBase}.feeds.{$feedName}.class"),
+                'type'          => config("{$configBase}.feeds.{$feedName}.type"),
+                'timestamp'     => strtotime($fields['TimeStamp']),
+                'information'   => json_encode($xmlReport),
+            ];
+
+            $events[] = $event;
         }
 
         return $this->success($events);
